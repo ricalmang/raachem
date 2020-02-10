5cbed589c8f8f890d70d2796d4b92b496ef6f0e9
Ricardo Almir Angnes <ricardo_almir@hotmail.com>
2020-02-10 14:04:47 -0200

    No comments

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
 	license="MIT",
     url="https://github.com/ricalmang/raachem",
-	install_requires = ["numpy","shutil"],
+	install_requires = ["numpy"],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
