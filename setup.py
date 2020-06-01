08b11272fda11d8262194621ac19985639270986
Ricardo Almir Angnes <ricardo_almir@hotmail.com>
2020-06-01 12:55:42 -0300

    Xyz geometry generation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="raachem",
-    version="0.0.5",
+    version="0.0.6",
     author="Ricardo Almir Angnes",
     author_email="ricardo_almir@hotmail.com",
     description="Tools I use for computational chemistry",
